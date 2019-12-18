9a8dfff85e9d42903392110ee924751322ebc11e
KGB33 <33pigbass@gmail.com>
2019-12-18 07:58:36 -0600

    fixed typos

--- CCSP/c5_GeneticAlgorithms/SimpleEquation.py
+++ CCSP/c5_GeneticAlgorithms/SimpleEquation.py
@@ -14,8 +14,8 @@ class SimpleEquation(Chromosome):
         return self.equation(self.x, self.y)
 
     @classmethod
-    def random_instance(cls):
-        return SimpleEquation(randrange(100), randrange(100))
+    def random_instance(cls, l=(lambda a, b: 6 * a - a * a + 4 * b - b * b)):
+        return SimpleEquation(randrange(100), randrange(100), l=l)
 
     def crossover(self, other):
         child1 = deepcopy(self)
@@ -27,21 +27,21 @@ class SimpleEquation(Chromosome):
     def mutate(self):
         if random() > 0.5:
             if random() > 0.5:
-                self.x = -1
+                self.x += 1
             else:
-                self.x = +1
+                self.x -= 1
         else:
             if random() > 0.5:
-                self.y = -1
+                self.y += 1
             else:
-                self.y = +1
+                self.y -= 1
 
     def __str__(self):
         return f"Fitness: {self.fitness()} for ({self.x}, {self.y})"
 
 
 if __name__ == "__main__":
-    inital_population = [SimpleEquation.random_instance() for _ in range(200)]
+    inital_population = [SimpleEquation.random_instance() for _ in range(20)]
     ga = GeneticAlgorithum(inital_population, threshold=13.0)
     result = ga.run()
     print(result)
